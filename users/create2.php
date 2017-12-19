7bf7ddfde62ab7db7e711b92e7283b78bef7a4c4
Jessie Klein <jmeredith.klein@gmail.com>
2017-12-18 19:37:53 -0700

    Getting database connection

--- users/create2.php
+++ users/create2.php
@@ -8,7 +8,16 @@
  //    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	// }
 	// echo $mysqli->host_info . "\n";
-	require (__DIR__.'/../connection.php');
+	//require (__DIR__.'/../connection.php');
+
+	$url = parse_url(getenv("mysql://b7acce26205a72:05a4c8c1@us-cdbr-iron-east-05.cleardb.net/heroku_58a0d489d10415d?reconnect=true"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$mysqli = new mysqli($server, $username, $password, $db);
 
 	$username = $mysqli->real_escape_string($_POST['username']);
 	$email = $mysqli->real_escape_string($_POST['email']);
