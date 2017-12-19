e267bb69bf190df5b0997cb4ac5788eeed400329
Jessie Klein <jmeredith.klein@gmail.com>
2017-12-18 19:27:22 -0700

    Setting up database connection.

--- connection.php
+++ connection.php
@@ -1,14 +1,10 @@
 <?php 
-	$servername = "us-cdbr-iron-east-05.cleardb.net";
-	$username = "b7acce26205a72";
-	$password = "05a4c8c1";
+	$url = parse_url(getenv("mysql://b7acce26205a72:05a4c8c1@us-cdbr-iron-east-05.cleardb.net/heroku_58a0d489d10415d?reconnect=true"));
 
-	// Create connection
-	$mysqli = new mysqli($servername, $username, $password);
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 
-	// Check connection
-	if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-	}
-	echo $mysqli->host_info . "\n";
+	$mysqli = new mysqli($server, $username, $password, $db);
 ?>
